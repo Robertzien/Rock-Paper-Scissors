5813ac42f230e2018414a07ca6c3ad1fee564b45
Robert <robertziengs03@gmail.com>
2022-06-09 15:43:14 +0200

    Changed code

--- script.js
+++ script.js
@@ -1,45 +1,20 @@
-//Randomly chooses rock, paper or scissors
-
-function computerPlay() {
-    let array = ["rock", "paper", "scissors"];
-    let item = array[Math.floor(Math.random() * array.length)];
-    return item;
-}
-
-//Plays a round of rock, paper, scissors
-    //Could probably be written smaller
-
-function playGame(playerSelection, computerSelection) {
-    //Both players pick the same
-    if (playerSelection === computerSelection) {
-        return("It's a draw!");
-        //Player picks rock
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return("Congratulations! Rock beats Scissors");
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return("You lose! Paper beats Rock");
-        //Player picks scissors
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return("Congratulations! Scissors beats Paper");    
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return("You lose! Rock beats Scissors");  
-        //Player picks paper
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return("Congratulations! Paper beats Rock");  
-    } else {
-        return("You lose! Scissors beats Paper");  
-    }
-}
-
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playGame(playerSelection, computerSelection));
+const choices = ["rock", "paper", "scissors"];
 
 function game() {
-    function playRound() {
-
-    }
+    playRound();
 }
 
+function playRound() {
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
+}
 
-
+function playerChoice() {
+    let input = prompt("Type Rock, Paper, or Scissors");
+    input = input.toLowerCase();
+    console.log(input);
+} 
+        
+function computerChoice() {
+    return choices[Math.floor(Math.random()*choices.length)];
+}
