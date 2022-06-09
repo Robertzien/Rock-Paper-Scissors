d86c8d5c370f65cb5d3a73be54059ebf870cb8d0
Robert <robertziengs03@gmail.com>
2022-06-09 16:10:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
 const choices = ["rock", "paper", "scissors"];
 
 function game() {
-    playRound();
+  playRound();
 }
 
 function playRound() {
-    const playerSelection = playerChoice();
-    const computerSelection = computerChoice();
+  const playerSelection = playerChoice();
+  const computerSelection = computerChoice();
+  const winner = checkWinner(playerSelection, computerSelection);
+  console.log(winner);
 }
 
 function playerChoice() {
-    let input = prompt("Type Rock, Paper, or Scissors");
+  let input = prompt("Type Rock, Paper, or Scissors");
+  while (input == null) {
+    input = prompt("Type Rock, Paper, or Scissors");
+  }
+  input = input.toLowerCase();
+  let check = validateInput(input);
+  while (check == false) {
+    input = prompt(
+      "Type Rock, Paper, or Scissors. Be sure that you spelled it right."
+    );
+    while (input == null) {
+      input = prompt("Type Rock, Paper, or Scissors");
+    }
     input = input.toLowerCase();
-    console.log(input);
-} 
-        
-function computerChoice() {
-    return choices[Math.floor(Math.random()*choices.length)];
+    check = validateInput(input);
+  }
+  return input;
 }
+
+function computerChoice() {
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function validateInput(choice) {
+  if (choices.includes(choice)) {
+    return true;
+  }
+  return false;
+}
+
+function checkWinner(choiceP, choiceC) {
+  if (choiceP == choiceC) {
+    return "Tie!";
+  } else if (
+    (choiceP == "rock" && choiceC == "scissors") ||
+    (choiceP == "paper" && choiceC == "rock") ||
+    (choiceP == "scissors" && choiceC == "paper")
+  ) {
+    return "Player wins!";
+  } else {
+    return "Computer wins!";
+  }
+}
+
+game();
